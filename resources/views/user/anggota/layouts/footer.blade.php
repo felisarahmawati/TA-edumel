4cdc62ca907362890ed8301c4c9eafb8e3cb0c21
Felisa Rahmawati <felisarahmawati6@gmail.com>
2023-05-07 14:47:08 +0700

    update footer

--- resources/views/user/anggota/layouts/footer.blade.php
+++ resources/views/user/anggota/layouts/footer.blade.php
@@ -8,62 +8,47 @@
 							<img src="/assets/user/images/logo-white.png" alt="" class="img-fluid">
 						</div>
 
-						<p class="mt-4">Edumel is a Bootstrap Template for online courses
-							education websites support multiple courses</p>
+						<p class="mt-4">Aplikasi L-Ebook menyediakan berbagai buku fiksi dan non - fiksi populer</p>
 
 						<div class="footer-socials mt-5">
 							<span class="me-2">Connect :</span>
-							<a href="#"><i class="fab fa-facebook-f"></i></a>
-							<a href="#"><i class="fab fa-twitter"></i></a>
-							<a href="#"><i class="fab fa-linkedin-in"></i></a>
-							<a href="#"><i class="fab fa-pinterest"></i></a>
+							<a href="https://instagram.com/perpusdamay?igshid=NTc4MTIwNjQ2YQ=="><i class="fab fa-instagram"></i></a>
+							<a href="https://youtu.be/yACWhCiBiRU"><i class="fab fa-youtube"></i></a>
 						</div>
 					</div>
 				</div>
 
 				<div class="col-xl-2 col-sm-4">
 					<div class="footer-widget mb-5 mb-lg-0">
-						<h5 class="widget-title">Explore</h5>
-						<ul class="list-unstyled footer-links">
-							<li><a href="#">About us</a></li>
-							<li><a href="#">Contact us</a></li>
-							<li><a href="#">Services</a></li>
-							<li><a href="#">Support</a></li>
-						</ul>
+						<h5 class="widget-title">Fasilitas</h5>
+                        <ul class="nospace clear latestimg">
+                            <li><a href="#"><img class="zoom" src="images/main/fasilitas1.jpg" alt=""></a></li>
+                            <li><a href="#"><img class="zoom" src="images/main/fasilitas2.jpg" alt=""></a></li>
+                            <li><a href="#"><img class="zoom" src="images/main/fasilitas3.jpg" alt=""></a></li>
+                            <li><a href="#"><img class="zoom" src="images/main/fasilitas4.jpg" alt=""></a></li>
+                            <li><a href="#"><img class="zoom" src="images/main/fasilitas5.jpg" alt=""></a></li>
+                            <li><a href="#"><img class="zoom" src="images/main/fasilitas6.jpg" alt=""></a></li>
+                        </ul>
 					</div>
 				</div>
 
-				<div class="col-xl-2 col-sm-4">
-					<div class="footer-widget mb-5 mb-lg-0">
-						<h5 class="widget-title ">Programs</h5>
-						<ul class="list-unstyled footer-links">
-							<li><a href="#">SEO Business</a></li>
-							<li><a href="#">Digital Marketing</a></li>
-							<li><a href="#">Graphic Design</a></li>
-							<li><a href="#">Social Marketing</a></li>
-						</ul>
-					</div>
-				</div>
+                {{-- <ul class="nospace clear latestimg">
+                    <li><a href="#"><img class="zoom" src="images/main/fasilitas1.jpg" alt=""></a></li>
+                    <li><a href="#"><img class="zoom" src="images/main/fasilitas2.jpg" alt=""></a></li>
+                    <li><a href="#"><img class="zoom" src="images/main/fasilitas3.jpg" alt=""></a></li>
+                    <li><a href="#"><img class="zoom" src="images/main/fasilitas4.jpg" alt=""></a></li>
+                    <li><a href="#"><img class="zoom" src="images/main/fasilitas5.jpg" alt=""></a></li>
+                    <li><a href="#"><img class="zoom" src="images/main/fasilitas6.jpg" alt=""></a></li>
+                </ul> --}}
 
-				<div class="col-xl-2 col-sm-4">
-					<div class="footer-widget mb-5 mb-lg-0">
-						<h5 class="widget-title">Links</h5>
-						<ul class="list-unstyled footer-links">
-							<li><a href="#">News & Blogs</a></li>
-							<li><a href="#">Privacy Policy</a></li>
-							<li><a href="#">Support</a></li>
-							<li><a href="#">Return Policy</a></li>
-						</ul>
-					</div>
-				</div>
 
-				<div class="col-xl-2 col-sm-4">
+				<div class="col-xl-4 col-sm-6">
 					<div class="footer-widget mb-5 mb-lg-0">
-						<h5 class="widget-title">Address</h5>
+						<h5 class="widget-title">Profil</h5>
 						<ul class="list-unstyled footer-links">
-							<li><a href="#">+0800 327 8534</a></li>
-							<li><a href="#">support@edumel.com</a></li>
-							<li><a href="#">123 Fifth Floor East 26th Street,New York, NY 10011</a></li>
+							<p class="btmspace-2">Alamat : Jl. Raya Pabean Udik, No. 15, Indramayu-Jawa Barat 45219</p>
+                            <p class="btmspace-2">Telp/Faks. +62 (0234) 276308/+62 (0234) 276308</p>
+                            <p class="btmspace-2">Lokasi : <a href="https://www.google.com/maps/place/6%C2%B019'01.7%22S+108%C2%B019'37.5%22E/@-6.3171197,108.3249053,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d-6.317125!4d108.327094" target="_blank">-6.317125, 108.327094</a></p>
 						</ul>
 					</div>
 				</div>
@@ -75,21 +60,19 @@
 		<div class="container">
 			<div class="row align-items-center">
 				<div class="col-xl-6 col-sm-12 col-lg-6">
-					<p class="mb-0 copyright text-sm-center text-lg-start">© 2021 Edumel All rights reserved by <a href="https://themeturn.com" rel="nofollow">Dreambuzz</a> </p>
+					<p class="mb-0 copyright text-sm-center text-lg-start">Copyright © 2023 - Perpus SMK Negeri 2 Indramayu</p>
 				</div>
 
 				<div class="col-xl-6 col-sm-12 col-lg-6">
-					<div class="footer-btm-links text-start text-sm-center text-lg-end">
-						<ul class="list-inline">
-							<li class="list-inline-item"><a href="#"> Terms of Service</a></li>
-							<li class="list-inline-item"><a href="#">Join Us</a></li>
-							<li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-						</ul>
-					</div>
+					<p class="footer-btm-links text-start text-sm-center text-lg-end">
+						Created by Admin Perpus SMK Negeri 2 Indramayu
+					</p>
 				</div>
 			</div>
 		</div>
 	</div>
+
+
 
 	<div class="fixed-btm-top">
 		<a href="#top-header" class="js-scroll-trigger scroll-to-top"><i class="fa fa-angle-up"></i></a>
